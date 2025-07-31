66e17ba4185bfb8ad43e95770df1bf6e063c2c64
Tyla-2025 <shyletvengesa072@gmail.com>
2025-07-31 18:19:53 +0200

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,13 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("name :", name)
+print("Data type of Name is", type(name))
+print("Age :", age)
+print("Data type of age is", type (age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
