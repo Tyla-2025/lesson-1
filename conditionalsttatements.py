2373c5489082ac2da73e6741f5e684a9046edc09
Tyla-2025 <shyletvengesa072@gmail.com>
2025-08-07 18:25:49 +0200

    Add files via upload

---
+++ conditionalsttatements.py
@@ -0,0 +1,11 @@
+number = int(input("Enter number to check"))
+print("Number to be checked :",number)
+
+if number>0:
+    print("This is a positive number")
+
+elif number<0:
+    print("This is a negetive number")
+
+else:
+    print("This is neutral")
